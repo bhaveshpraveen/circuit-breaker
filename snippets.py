49ccf311eea4393936217652d3a2a16772a96e36
bhavesh <bhaveshpraveen10@gmail.com>
2020-12-13 06:58:12 +0530

    Add snippets

---
+++ snippets.py
@@ -0,0 +1,21 @@
+import http
+
+import requests
+
+faulty_endpoint = "http://localhost:5000/failure"
+success_endpoint = "http://localhost:5000/success"
+random_status_endpoint = "http://localhost:5000/random"
+
+
+def make_request(url):
+    try:
+        response = requests.get(url, timeout=0.3)
+        if response.status_code == http.HTTPStatus.OK:
+            print(f"Call to {url} succeed with status code = {response.status_code}")
+            return response
+        if 500 <= response.status_code < 600:
+            print(f"Call to {url} failed with status code = {response.status_code}")
+            raise Exception("Server Issue")
+    except Exception:
+        print(f"Call to {url} failed")
+        raise
